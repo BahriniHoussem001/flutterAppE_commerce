cc53d41b86711e1179ca69c8ea5bb6c167c8ad6a
BahriniHoussem001 <145834601+BahriniHoussem001@users.noreply.github.com>
2026-03-02 16:57:19 +0100

    configuration et connextion au Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'theme/app_theme.dart';
 import 'pages/preview_ui_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
